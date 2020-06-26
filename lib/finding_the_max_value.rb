fff3a53ba91951658f69a1ce17baf54385748023
Gregory A.S. Wright <geslim@gmail.com>
2020-06-26 15:00:30 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -2,7 +2,9 @@ def find_max_value(array)
   count = 0
   array.sort!
   while count <= array.length do
-    puts array
+    if count == array.last
+    return array.last
+    end
     count += 1
   end
 end
